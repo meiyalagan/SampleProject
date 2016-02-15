80ecfb539ad8ac3d6cb726fa0a36421b320dc5ef
Meiy <administrator@Meiys-Macmini.local>
2016-02-15 19:00:25 +0530

    Organizer UIView Screen Class Added

---
+++ SportsApp/OrganizerViewController.m
@@ -0,0 +1,37 @@
+//
+//  OrganizerViewController.m
+//  SportsApp
+//
+//  Created by administrator on 2/15/16.
+//
+//
+
+#import "OrganizerViewController.h"
+
+@interface OrganizerViewController ()
+
+@end
+
+@implementation OrganizerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
