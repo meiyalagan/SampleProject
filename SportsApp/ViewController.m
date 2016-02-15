e68eff343ceeac87d19b25bc6d26c726992e8c95
sarathkumar s <sarathkumar@Saraths-Mac-mini.local>
2016-02-15 15:15:25 +0530

    change nslog

--- SportsApp/ViewController.m
+++ SportsApp/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"\n\n hai Meiy");
+    NSLog(@"\n\n hai SARAATH");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
