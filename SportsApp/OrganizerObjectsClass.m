0036636ceeb4c5b30bd2ce24860a9036e5e4fe61
Meiy <administrator@Meiys-Macmini.local>
2016-02-16 10:34:58 +0530

    SliderCustomView

---
+++ SportsApp/OrganizerObjectsClass.m
@@ -0,0 +1,13 @@
+//
+//  OrganizerObjectsClass.m
+//  SportsApp
+//
+//  Created by administrator on 2/16/16.
+//
+//
+
+#import "OrganizerObjectsClass.h"
+
+@implementation OrganizerObjectsClass
+
+@end
---
+++ SportsApp/SliderCustomView.m
@@ -0,0 +1,21 @@
+//
+//  SliderCustomView.m
+//  SportsApp
+//
+//  Created by administrator on 2/16/16.
+//
+//
+
+#import "SliderCustomView.h"
+
+@implementation SliderCustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
